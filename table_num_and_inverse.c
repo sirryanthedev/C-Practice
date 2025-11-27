7491ff1d33e375681fb3dd3a3396333b5883e4ae
Ryan <ryanvermeulen08@outlook.com>
2025-11-27 19:41:08 +0100

    program which counts from 1 to 12 and prints each number with its inverse ex. (1.6.3)

---
+++ table_num_and_inverse.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+
+                                                    // a program which counts from 1 to 12 and prints each number with its inverse (1.6.3)
+
+    printf(" n | 1/n\n");
+    printf("------------\n");
+    for (int i = 1; i <= 12; i++){
+        float inverse = 1.0f/i;
+        printf("%2d | %.5f\n", i, inverse);
+    }
+
+    return 0;
+}
