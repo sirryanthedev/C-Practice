18e4d627611f7d5a4d1709db1d58bac9d412c9d7
Ryan <ryanvermeulen08@outlook.com>
2025-11-27 12:39:54 +0100

    Program which prints a number of rows growing from 1 to specified value less than 10

---
+++ growing_rows_lt10.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#define ROWS 5                       // choose a value (<10) for rows - replace 5 with a value (<10)
+
+int main(){
+
+    for(int i = 1; i <= ROWS; i++){
+        
+        for(int s = ROWS-i; s > 0; s--){
+            printf(" ");
+        }
+
+        for(int j = i; j >= 1; j--){
+            printf("%d", j);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
