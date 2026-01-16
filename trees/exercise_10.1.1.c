9b236e90be9e717d91352fdc1aa0c36b59a582de
Ryan <ryanvermeulen08@outlook.com>
2026-01-16 18:58:54 +0100

    create a binary tree node struct

---
+++ trees/exercise_10.1.1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node_bt{
+    int data; // data to store
+    struct node_bt* left; // left child
+    struct node_bt* right; // right child
+};
+
+int main(){
+    return 0;
+}
