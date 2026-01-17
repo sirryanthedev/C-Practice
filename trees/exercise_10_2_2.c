f8f00e080a12b755cf1259d3cbe77c1774674e24
Ryan <ryanvermeulen08@outlook.com>
2026-01-17 16:54:32 +0100

    add struct architecture for integer trees

---
+++ trees/exercise_10_2_2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// architecture for integer trees
+typedef struct node_t{
+    int data; // value of the node
+    struct node_t** children; // dynamically allocated array of pointers to children
+}node_t;
+
+int main(){
+    return 0;
+}
